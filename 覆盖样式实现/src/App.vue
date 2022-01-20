8860667fc206ce379e3fb6b07c4a45e45e7b02b5
yangmengwei925 <58463843+yangmengwei925@users.noreply.github.com>
2022-01-20 13:30:18 +0800

    first commit

---
+++ 覆盖样式实现/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  methods: {},
+};
+</script>
+
+<style lang="scss">
+@import "./assets/scss/index.scss";
+
+[data-theme="dark"] {
+  body {
+    background: $dark-fill-1;
+  }
+  .reaconmend .reaconmend-list .item .name {
+    color: $dark-color-text;
+  }
+  .reaconmend .reaconmend-list .item .desc {
+    color: $dark-color-text-1;
+  }
+  .header .text {
+    color: $dark-color-text-2;
+  }
+}
+</style>
